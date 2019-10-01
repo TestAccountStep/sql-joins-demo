afc1ea483294469aa22c851c4f8a8aac2362958f
Juthes <Juthes@ex.ua>
2019-10-01 20:43:09 +0300

    JoinsHM

---
+++ SqlJoinsDemo/Scripts/Script1.sql
@@ -0,0 +1,10 @@
+﻿SELECT c.[name] AS N'Город', s.[name] AS 'ПоставщикАДЫН', s2.[name] AS 'ПоставщикДВА' FROM Cities AS c
+JOIN
+Suppliers AS s
+ON s.city_id = c.id
+JOIN
+Suppliers AS s2
+ON s2.city_id = s.city_id
+WHERE s.id > s2.id;
+
+
